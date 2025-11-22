8dbece974b08e86ff8fc74074285600ff779c4d2
Kaj Wiik <kaj.wiik@utu.fi>
2025-11-22 23:36:08 +0200

    Add @pywrapper and Network

--- src/ScikitRF.jl
+++ src/ScikitRF.jl
@@ -6,12 +6,32 @@ using Reexport
 
 const skrf = PythonCall.pynew()
 
+ScikitRF() = PythonCall.pynew()
+
 function __init__()
     PythonCall.pycopy!(skrf, pyimport("skrf"))
 end
 
-export skrf
 
-greet() = print("Hello World!")
+macro pywrapper(typename, pytype)
+    quote
+        struct $(esc(typename))
+            py::Py
+        end
+        
+        Base.getproperty(x::$(esc(typename)), name::Symbol) = 
+            name === :py ? getfield(x, :py) : getproperty(getfield(x, :py), name)
+        
+        Base.convert(::Type{Py}, x::$(esc(typename))) = getfield(x, :py)
+        Base.convert(::Type{$(esc(typename))}, x::Py) = $(esc(typename))(x)
+    end
+end
+
+@pywrapper Network Network
+
+Network() = Network(skrf.Network())
+
+export skrf, ScikitRF, Network
+
 
 end # module ScikitRF
